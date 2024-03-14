d36e8919bfe53aa798c891dee80d16c0e92fd140
Rajat Jindal <rajatjindal83@gmail.com>
2024-03-14 10:28:15 +0530

    dont forget listener for a second
    Signed-off-by: Rajat Jindal <rajatjindal83@gmail.com>

--- crates/shim/src/synchronous/mod.rs
+++ crates/shim/src/synchronous/mod.rs
@@ -507,7 +507,7 @@ pub fn spawn(opts: StartOpts, grouping: &str, vars: Vec<(&str, &str)>) -> Result
             .map_err(io_error!(e, "spawn shim"))
             .map(|child| {
                 // Ownership of `listener` has been passed to child.
-                std::mem::forget(_listener);
+                //std::mem::forget(_listener);
                 (child.id(), address)
             })
     }
