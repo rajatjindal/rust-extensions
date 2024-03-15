1cb5e1501d9a8c95d039792e8b27ae2f9e68e02d
Rajat Jindal <rajatjindal83@gmail.com>
2024-03-15 07:23:33 +0530

    debug mroe
    Signed-off-by: Rajat Jindal <rajatjindal83@gmail.com>

--- crates/shim/src/cgroup.rs
+++ crates/shim/src/cgroup.rs
@@ -97,80 +97,80 @@ fn write_process_oom_score(pid: u32, score: i64) -> Result<()> {
 pub fn collect_metrics(pid: u32) -> Result<Metrics> {
     let mut metrics = Metrics::new();
 
-    let cgroup = get_cgroup(pid)?;
+    // let cgroup = get_cgroup(pid)?;
 
-    // to make it easy, fill the necessary metrics only.
-    for sub_system in Cgroup::subsystems(&cgroup) {
-        match sub_system {
-            Subsystem::Cpu(cpu_ctr) => {
-                let mut cpu_usage = CPUUsage::new();
-                let mut throttle = Throttle::new();
-                let stat = cpu_ctr.cpu().stat;
-                for line in stat.lines() {
-                    let parts = line.split(' ').collect::<Vec<&str>>();
-                    if parts.len() != 2 {
-                        Err(Error::Other(format!("invalid cpu stat line: {}", line)))?;
-                    }
+    // // to make it easy, fill the necessary metrics only.
+    // for sub_system in Cgroup::subsystems(&cgroup) {
+    //     match sub_system {
+    //         Subsystem::Cpu(cpu_ctr) => {
+    //             let mut cpu_usage = CPUUsage::new();
+    //             let mut throttle = Throttle::new();
+    //             let stat = cpu_ctr.cpu().stat;
+    //             for line in stat.lines() {
+    //                 let parts = line.split(' ').collect::<Vec<&str>>();
+    //                 if parts.len() != 2 {
+    //                     Err(Error::Other(format!("invalid cpu stat line: {}", line)))?;
+    //                 }
 
-                    // https://github.com/opencontainers/runc/blob/dbe8434359ca35af1c1e10df42b1f4391c1e1010/libcontainer/cgroups/fs2/cpu.go#L70
-                    match parts[0] {
-                        "usage_usec" => {
-                            cpu_usage.set_total(parts[1].parse::<u64>().unwrap());
-                        }
-                        "user_usec" => {
-                            cpu_usage.set_user(parts[1].parse::<u64>().unwrap());
-                        }
-                        "system_usec" => {
-                            cpu_usage.set_kernel(parts[1].parse::<u64>().unwrap());
-                        }
-                        "nr_periods" => {
-                            throttle.set_periods(parts[1].parse::<u64>().unwrap());
-                        }
-                        "nr_throttled" => {
-                            throttle.set_throttled_periods(parts[1].parse::<u64>().unwrap());
-                        }
-                        "throttled_usec" => {
-                            throttle.set_throttled_time(parts[1].parse::<u64>().unwrap());
-                        }
-                        _ => {}
-                    }
-                }
-                let mut cpu_stats = CPUStat::new();
-                cpu_stats.set_throttling(throttle);
-                cpu_stats.set_usage(cpu_usage);
-                metrics.set_cpu(cpu_stats);
-            }
-            Subsystem::Mem(mem_ctr) => {
-                let mem = mem_ctr.memory_stat();
-                let mut mem_entry = MemoryEntry::new();
-                mem_entry.set_usage(mem.usage_in_bytes);
-                let mut mem_stat = MemoryStat::new();
-                mem_stat.set_usage(mem_entry);
-                mem_stat.set_total_inactive_file(mem.stat.total_inactive_file);
-                metrics.set_memory(mem_stat);
-            }
-            Subsystem::Pid(pid_ctr) => {
-                let mut pid_stats = PidsStat::new();
-                pid_stats.set_current(
-                    pid_ctr
-                        .get_pid_current()
-                        .map_err(other_error!(e, "get current pid"))?,
-                );
-                pid_stats.set_limit(
-                    pid_ctr
-                        .get_pid_max()
-                        .map(|val| match val {
-                            // See https://github.com/opencontainers/runc/blob/dbe8434359ca35af1c1e10df42b1f4391c1e1010/libcontainer/cgroups/fs/pids.go#L55
-                            cgroups_rs::MaxValue::Max => 0,
-                            cgroups_rs::MaxValue::Value(val) => val as u64,
-                        })
-                        .map_err(other_error!(e, "get pid limit"))?,
-                );
-                metrics.set_pids(pid_stats)
-            }
-            _ => {}
-        }
-    }
+    //                 // https://github.com/opencontainers/runc/blob/dbe8434359ca35af1c1e10df42b1f4391c1e1010/libcontainer/cgroups/fs2/cpu.go#L70
+    //                 match parts[0] {
+    //                     "usage_usec" => {
+    //                         cpu_usage.set_total(parts[1].parse::<u64>().unwrap());
+    //                     }
+    //                     "user_usec" => {
+    //                         cpu_usage.set_user(parts[1].parse::<u64>().unwrap());
+    //                     }
+    //                     "system_usec" => {
+    //                         cpu_usage.set_kernel(parts[1].parse::<u64>().unwrap());
+    //                     }
+    //                     "nr_periods" => {
+    //                         throttle.set_periods(parts[1].parse::<u64>().unwrap());
+    //                     }
+    //                     "nr_throttled" => {
+    //                         throttle.set_throttled_periods(parts[1].parse::<u64>().unwrap());
+    //                     }
+    //                     "throttled_usec" => {
+    //                         throttle.set_throttled_time(parts[1].parse::<u64>().unwrap());
+    //                     }
+    //                     _ => {}
+    //                 }
+    //             }
+    //             let mut cpu_stats = CPUStat::new();
+    //             cpu_stats.set_throttling(throttle);
+    //             cpu_stats.set_usage(cpu_usage);
+    //             metrics.set_cpu(cpu_stats);
+    //         }
+    //         Subsystem::Mem(mem_ctr) => {
+    //             let mem = mem_ctr.memory_stat();
+    //             let mut mem_entry = MemoryEntry::new();
+    //             mem_entry.set_usage(mem.usage_in_bytes);
+    //             let mut mem_stat = MemoryStat::new();
+    //             mem_stat.set_usage(mem_entry);
+    //             mem_stat.set_total_inactive_file(mem.stat.total_inactive_file);
+    //             metrics.set_memory(mem_stat);
+    //         }
+    //         Subsystem::Pid(pid_ctr) => {
+    //             let mut pid_stats = PidsStat::new();
+    //             pid_stats.set_current(
+    //                 pid_ctr
+    //                     .get_pid_current()
+    //                     .map_err(other_error!(e, "get current pid"))?,
+    //             );
+    //             pid_stats.set_limit(
+    //                 pid_ctr
+    //                     .get_pid_max()
+    //                     .map(|val| match val {
+    //                         // See https://github.com/opencontainers/runc/blob/dbe8434359ca35af1c1e10df42b1f4391c1e1010/libcontainer/cgroups/fs/pids.go#L55
+    //                         cgroups_rs::MaxValue::Max => 0,
+    //                         cgroups_rs::MaxValue::Value(val) => val as u64,
+    //                     })
+    //                     .map_err(other_error!(e, "get pid limit"))?,
+    //             );
+    //             metrics.set_pids(pid_stats)
+    //         }
+    //         _ => {}
+    //     }
+    // }
     Ok(metrics)
 }
 
